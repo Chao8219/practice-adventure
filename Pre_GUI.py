1ee334b112ab503efbc788c61af339fdc705563c
Zichao Hou <39388321+Chao8219@users.noreply.github.com>
2018-05-24 00:37:14 -0400

    Update Input Widget
    The input widget is ready to use. The next step is to organize the code.

--- Pre_GUI.py
+++ Pre_GUI.py
@@ -1,11 +1,18 @@
 from tkinter import *
 import time
 
+user_sub='' # set a string to store user's input
+entered=0 # check if user type in anything through Entry
+
 def endWindow():
+    window.quit()
     window.destroy()
     return
 
-def TypeInLine(Line):
+def TypeLine(Line):
+    global entered
+    entered=0 # with new line, the user is not entering
+    type_in.config(state=DISABLED) # not allowed entering between lines
     size=len(Line)
     for i in range(0,size):
         script.config(state=NORMAL)
@@ -13,11 +20,25 @@ def TypeInLine(Line):
         script.see(END) # auto scroll the scrollbar
         script.config(state=DISABLED)
         window.update()
-        time.sleep(0.1)
+        window.after(100) # delay 100 ms
+    window.after(400)
+    window.update()
+    type_in.config(state=NORMAL)
     return
 
-def getText():
-    print('Anything')
+def getText(event=None):
+    global entered,user_sub
+    user_sub=type_in.get()
+    print('The input is '+user_sub)
+    entered=1
+    type_in.delete(0,END)
+    window.update()
+    return
+
+def wait_for_input():
+    while(entered==0):
+        print('waiting...')
+        window.update()
     return
 
 # ---- General Initialization ---- #
@@ -42,18 +63,37 @@ exit_button.place(anchor=S,x=300,y=490,height=40,width=50)
 
 # ----- Entry Initialization ---- #
 type_in=Entry(window)
+type_in.bind('<Return>',getText)
 type_in.pack()
 type_in.place(anchor=S,x=300,y=450,width=200)
 
-Line_test='Welcome to the GUI debug world.\n'
-TypeInLine(Line_test)
-Line_test2='I am your host, Will.\n'
-TypeInLine(Line_test2)
-Line_test3='Many bugs may appear in this world, such as xhckjendksdjck and u4iwksjdcuhe2uo3432\n'
-TypeInLine(Line_test3)
-Line_test4='But,\n'+'so far\n'+'so good.\n'
-TypeInLine(Line_test4)
-Line_test5='I wonder\n'+'if\n'+'we\n'+'simply\n'+'create\n'+'many\n'+'lines\n'+'Then\n'+'what\n'+'would\n'+'happen?\n'
-TypeInLine(Line_test5)
+# ---- <Test Texts> ---- #
+##Line_test='Welcome to the GUI debug world.\n'
+##TypeInLine(Line_test)
+##Line_test2='I am your host, Will.\n'
+##TypeInLine(Line_test2)
+##Line_test3='Many bugs may appear in this world, such as xhckjendksdjck and u4iwksjdcuhe2uo3432\n'
+##TypeInLine(Line_test3)
+##Line_test4='But,\n'+'so far\n'+'so good.\n'
+##TypeInLine(Line_test4)
+##Line_test5='I wonder\n'+'if\n'+'we\n'+'simply\n'+'create\n'+'many\n'+'lines\n'+'Then\n'+'what\n'+'would\n'+'happen?\n'
+##TypeInLine(Line_test5)
+# ---- </Test Texts> ---- #
+
+# ---- <Test Texts with input> ---- #
+while(1):
+    Line1='Who are you?\n'
+    TypeLine(Line1)
+    wait_for_input()
+    if(user_sub=='Jack' or user_sub=='jack'):
+        TypeLine('Jack, welcome back.\n')
+        break
+    elif(user_sub!=''):
+        TypeLine('Are you sure, Jack?\n')
+    else:
+        TypeLine('Please type anything.\n')
+
+TypeLine('I am your host, Alex.\n')
+# ---- </Test Texts with input> ---- #
 
 window.mainloop()
