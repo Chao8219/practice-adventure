337c5fe1d5e60c20d48f0ec4d6ef330e3ca69b85
Zichao Hou <zichao@gwu.edu>
2019-06-15 15:29:56 -0400

    creature class updated

--- creatures_class.py
+++ creatures_class.py
@@ -1,11 +1,14 @@
 import numpy
 import random
+import object_read
 
-class CreateGeneralBeing:
+class BuildCreature:
     """
     The general idea is, every creature in this world has the following 
-    attributes:
-    name, level, attack, defense, speed, hp, drops.
+    ten attributes:
+    id, name, attack, defense, speed, hp, skill 1, skill 2, drop 1, drop 2.
+
+    Additionally, the object should also have level attribute.
 
     The same creature with same level may have the same base attributes, 
     but they may have various random tiny difference.
@@ -16,14 +19,38 @@ class CreateGeneralBeing:
 
     Just remember to del the object once finish using.
     """
-    def __init__(self):
-        pass
+
+    def __init__(self, file_path, creature_id, creature_level=1,):
+        """
+        Constructor
+
+        Parameters
+        ----------
+        file_path: string
+            The relative location of the db file.
+
+        creature_id: integer
+            The unique creature id.
+
+        creature_level: integer or string
+            The creature level.
+        """
+        self.id = creature_id
+        self.level = creature_level
+        one_creature = object_read.read_one_creature_by_id(file_path, 
+                                                            creature_id)
+        self.name = one_creature[1]
+        self.attack = one_creature[2]
+        self.defense = one_creature[3]
+        self.speed = one_creature[4]
+        self.hp = one_creature[5]
+        self.skill1 = one_creature[6]
+        self.skill2 = one_creature[7]
+        self.drop1 = one_creature[8]
+        self.drop2 = one_creature[9]
 
     def __del__(self):
         pass
-
-class CreateFoulBeing(CreateGeneralBeing):
-    pass
 
 if __name__ == '__main__':
     print('Please import this module to instantiate classes.')
--- object_read.py
+++ object_read.py
@@ -27,7 +27,7 @@ def read_all_creature(file_path):
     print('Database read successfully')
     return all_creature_info
 
-def read_one_creature(file_path, creature_name):
+def read_one_creature_by_name(file_path, creature_name):
     """
     Method to read one creature data by the given creature name.
 
@@ -49,6 +49,39 @@ def read_one_creature(file_path, creature_name):
         cur = conn.cursor()
         cur.execute('SELECT * FROM creature_info WHERE NAME=?', 
                     (creature_name, ))
+    except sqlite3.OperationalError as err:
+        print(err)
+        print('It is possible that the provided file path is wrong.')
+        return None
+    creature_data = cur.fetchone()
+    conn.close()
+    if creature_data is None:
+        print('The creature name that is passed in does not' + 
+              'exist in the db file')
+    return creature_data
+
+def read_one_creature_by_id(file_path, creature_id):
+    """
+    Method to read one creature data by the given creature name.
+
+    Parameters
+    ----------
+    file_path: string
+        The relative db file path
+
+    creature_id: string
+        The string id that the code is trying to read.
+    
+    Returns
+    -------
+    creature_data: tuple
+        The tuple that contains all info about this creature.
+    """
+    try:
+        conn = sqlite3.connect(file_path)
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM creature_info WHERE ID=?', 
+                    (creature_id, ))
     except sqlite3.OperationalError as err:
         print(err)
         print('It is possible that the provided file path is wrong.')
